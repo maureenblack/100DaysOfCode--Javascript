2f70dd269f61d7bf8bf913cc784ae5b7a091b55d
maureenblack <maureenwepngong@gmail.com>
2022-08-04 21:58:55 +0100

    #day2

---
+++ 100days.js
@@ -0,0 +1,11 @@
+
+const monthlyRent = 500;
+let yearlyRent = monthlyRent + 12;
+console.log(yearlyRent);
+
+yearlyRent = 1;
+console.log(yearlyRent);
+const firstName = "Maureen";
+const lastName = "Wepngong. ";
+const sentence = "Hello, " + firstName + " " + lastName + "How are you?!";
+console.log(sentence);
--- Day1.js
+++
@@ -1,3 +0,0 @@
-let myFirstVariable = 1;
-myFirstVariable = 'Some string';
-myFirstVariable = new SomeComplexClass();
